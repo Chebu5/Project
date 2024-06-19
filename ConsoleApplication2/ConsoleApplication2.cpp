1d96e24fa8529273d6be15c535d3c8bafdd960f9
Егор <egora8262@gmail.com>
2024-06-19 18:11:47 +0800

    Удаление дубликатов

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <stdio.h>
+#include <windows.h>
 using namespace std;
 namespace fs = std::filesystem;
 std::string file_hash(const std::string& path) {
@@ -15,17 +17,26 @@ std::string file_hash(const std::string& path) {
 }
 
 int main() {
-    setlocale(LC_ALL, "RU");
+    string director_path;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     std::unordered_map<std::string, std::string> hashes;
-    //Установить свой путь к папке
-    std::string directory_path = "D:\\Код для Vs\\ConsoleApplication2\\ConsoleApplication2\\Папка1";
+    std::string directory_path = "D:\\ConsoleApplication2\\ConsoleApplication2\\Папка1";
+
     for (const auto& entry : fs::recursive_directory_iterator(directory_path)) {
         if (fs::is_regular_file(entry.status())) {
             std::string file_path = entry.path().string();
             std::string hash = file_hash(file_path);
 
             if (hashes.find(hash) != hashes.end()) {
-                std::cout << "Найдены дубликаты: " << endl << file_path << endl << " и " << endl << hashes[hash] << std::endl;
+                std::cout << "Найдены дубликаты: " << endl << "1." << file_path << endl << " и " << endl << "2." << hashes[hash] << std::endl;
+                cout << "Введите путь к файлу который хотите удалить: ";
+                getline(cin, director_path);
+                if (fs::remove(director_path) != 0)
+                    cout << "Файл успешно удалён" << endl;
+                else
+                    cout << "Ошибка удаления файла" << endl;
+
             }
             else {
                 hashes[hash] = file_path;
@@ -34,4 +45,5 @@ int main() {
     }
 
     return 0;
+
 }
