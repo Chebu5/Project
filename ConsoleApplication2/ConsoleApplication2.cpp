52995e2f202370e11ce3118fa2b754f1383baad2
Егор <egora8262@gmail.com>
2024-06-19 18:09:26 +0800

    Поиск дубликатов

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -6,21 +6,32 @@
 #include <sstream>
 #include <string>
 using namespace std;
-void get_files(vector<string>& paths, const string& current_path) {
-	for (const auto& file : filesystem::directory_iterator(current_path)) {
-		if (filesystem::is_directory(file)) {
-			get_files(paths, file.path().string());
-		}
-		else {
-			paths.push_back(file.path().string());
-		}
-	}
+namespace fs = std::filesystem;
+std::string file_hash(const std::string& path) {
+    std::ifstream file(path, std::ios::binary);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return std::to_string(std::hash<std::string>{}(buffer.str()));
 }
+
 int main() {
-	setlocale(LC_ALL, "RU");
-	vector<string>paths;
-	get_files(paths, "D:\\Код для Vs\\ConsoleApplication2\\ConsoleApplication2");
-	for (int i = 0; i < paths.size(); i++) {
-		cout << paths[i] << endl;
-	}
+    setlocale(LC_ALL, "RU");
+    std::unordered_map<std::string, std::string> hashes;
+    //Установить свой путь к папке
+    std::string directory_path = "D:\\Код для Vs\\ConsoleApplication2\\ConsoleApplication2\\Папка1";
+    for (const auto& entry : fs::recursive_directory_iterator(directory_path)) {
+        if (fs::is_regular_file(entry.status())) {
+            std::string file_path = entry.path().string();
+            std::string hash = file_hash(file_path);
+
+            if (hashes.find(hash) != hashes.end()) {
+                std::cout << "Найдены дубликаты: " << endl << file_path << endl << " и " << endl << hashes[hash] << std::endl;
+            }
+            else {
+                hashes[hash] = file_path;
+            }
+        }
+    }
+
+    return 0;
 }
