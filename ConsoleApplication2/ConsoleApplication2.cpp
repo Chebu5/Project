44f9f5d2ceb0b302f701f92d555a6a25efcc223d
Егор <@LAPTOP-B6DSN8AQ>
2024-06-19 18:03:32 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+void get_files(vector<string>& paths, const string& current_path) {
+	for (const auto& file : filesystem::directory_iterator(current_path)) {
+		if (filesystem::is_directory(file)) {
+			get_files(paths, file.path().string());
+		}
+		else {
+			paths.push_back(file.path().string());
+		}
+	}
+}
+int main() {
+	setlocale(LC_ALL, "RU");
+	vector<string>paths;
+	get_files(paths, "D:\\Код для Vs\\ConsoleApplication2\\ConsoleApplication2");
+	for (int i = 0; i < paths.size(); i++) {
+		cout << paths[i] << endl;
+	}
+}
